bcbd8dbcc785d44a6e95a7bf31036488f4e0fab3
tirasYusop <yusopnoratira@gmail.com>
2026-04-16 23:39:14 +0800

    make it undermaintanance

---
+++ src/pages/Maintenance.jsx
@@ -0,0 +1,8 @@
+export default function Maintenance() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>🚧 Under Maintenance</h1>
+      <p>We are currently updating the system. Please come back later.</p>
+    </div>
+  );
+}
